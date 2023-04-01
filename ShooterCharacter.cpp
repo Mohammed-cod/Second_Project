2479eae9a8da0f2f4bf37c5eedce44ecc75e69e6
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-04-01 12:34:14 +0300

    Spring Arm
    - USpringArmComponent
    - CreateDefaultSubobject<>()
    - SetupAttachment()
    - TargetArmLength
    - bUsePawnControlRotation

--- ShooterCharacter.cpp
+++ ShooterCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
@@ -9,6 +10,11 @@ AShooterCharacter::AShooterCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	//Create a CameraBoom(pulls in towards the character if there is a collision) 
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CamraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f; //The camera follows at this distatnce behind the character
+	CameraBoom->bUsePawnControlRotation = true;//Rotate the arm based in the controller. Whenever our pawn controller move, the spring arm is going to use that rotation 
 }
 
 // Called when the game starts or when spawned
