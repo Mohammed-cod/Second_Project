4cf38e873fa8c5daa5a2888dd301f2fa03875db0
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-04-02 03:18:37 +0300

    Animation(Creating AnimInstace)
    - UpdateAnimationProperties()
    - NativeInitializeAnimation()
    - TryGetPawnOwner()
    - GetVelocity()
    - Velocity.Size()
    - GetCharacterMovement()
    - IsFalling()
    - GetCurrentAcceleration()
    -

---
+++ ShooterAnimInstance.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);//more like a Tick func 
+
+
+	virtual void NativeInitializeAnimation() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	class AShooterCharacter* ShooterCharacter;//reference to character
+
+	/**The speed of the character*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	/**Whether or not the character is in air*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	/**Whether or not the character is moving*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+	
+};
