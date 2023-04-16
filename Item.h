f10b6541d951aca31941844d5882244a15a37302
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-04-17 02:14:58 +0300

    Pickup widget
    -

--- Item.h
+++ Item.h
@@ -33,5 +33,8 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	class UBoxComponent* CollisionBox;
 
+	/**Popup widget for when the player looks at the item*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
 public:
 };
