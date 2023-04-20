95f9f5345da213b12b8cf4f15c9cbcd8d1152cbf
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-04-20 05:40:16 +0300

    Bind Item Name
    - Bind Item Name
    - Bind Item Count
    - Bind Star Opacity

--- Item.cpp
+++ Item.cpp
@@ -8,7 +8,9 @@
 #include "ShooterCharacter.h"
 
 // Sets default values
-AItem::AItem()
+AItem::AItem() : ItemName(FString("Default")), 
+				 ItemCount(0),
+				 ItemRarity(EItemRarity::EIR_Common)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -34,8 +36,14 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 
 	//Hide Pickup Widget
-	PickupWidget->SetVisibility(false);//Hidden by default
+	if(PickupWidget)
+	{
+		PickupWidget->SetVisibility(false);//Hidden by default
+	}
 
+	// Sets ActiveStars array based on Item Rarity
+	SetActiveStars();
+	
 	// Setup overlap for AreaSphere
 	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnSphereOverlap);
 	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AItem::OnSphereEndOverlap);
@@ -63,6 +71,44 @@ void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor*
 		{
 			ShooterCharacter->IncrementOverlappedItemCount(-1);
 		}
+	}
+}
+
+void AItem::SetActiveStars()
+{
+	// The 0 element isn't used
+	for (int32 i = 0; i <= 5; i++)
+	{
+		ActiveStars.Add(false);
+	}
+
+	switch (ItemRarity)
+	{
+	case EItemRarity::EIR_Damaged:
+		ActiveStars[1] = true;
+		break;
+	case EItemRarity::EIR_Common:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		break;
+	case EItemRarity::EIR_Uncommon:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		break;
+	case EItemRarity::EIR_Rare:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		ActiveStars[4] = true;
+		break;
+	case EItemRarity::EIR_Legendary:
+		ActiveStars[1] = true;
+		ActiveStars[2] = true;
+		ActiveStars[3] = true;
+		ActiveStars[4] = true;
+		ActiveStars[5] = true;
+		break;
 	}
 }
 
