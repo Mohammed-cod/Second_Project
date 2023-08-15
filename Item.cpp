94ca4ba718ca0e7773d5dfd3783dea6456a061d1
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-08-15 15:15:05 +0300

    Show Outline While Interping

--- Item.cpp
+++ Item.cpp
@@ -24,7 +24,8 @@ AItem::AItem() : ItemName(FString("Default")),
 				 InterpInitialYawOffset(0.f),
 				 ItemType(EItemType::EIT_MAX),
 				 InterpLocIndex(0),
-				 MaterialIndex(0)
+				 MaterialIndex(0),
+				 bCanChangeCustomDepth(true)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -209,6 +210,7 @@ void AItem::FinishInterping()
 	SetActorScale3D(FVector(1.f));
 
 	DisableGlowMaterial();
+	bCanChangeCustomDepth = true;
 	DisableCustomDepth();
 }
 
@@ -286,12 +288,18 @@ FVector AItem::GetInterpLocation()
 
 void AItem::EnableCustomDepth()
 {
-	ItemMesh->SetRenderCustomDepth(true);
+	if(bCanChangeCustomDepth)
+	{
+		ItemMesh->SetRenderCustomDepth(true);
+	}
 }
 
 void AItem::DisableCustomDepth()
 {
+	if(bCanChangeCustomDepth)
+	{
 	ItemMesh->SetRenderCustomDepth(false);
+	}
 }
 
 void AItem::InitializeCustomDepth()
@@ -364,4 +372,6 @@ void AItem::StartItemCurve(AShooterCharacter* Char)
 	const float ItemRotationYaw{ GetActorRotation().Yaw };
 	// Initial Yaw offset between Camera and Item
 	InterpInitialYawOffset = ItemRotationYaw - CameraRotationYaw;
+
+	bCanChangeCustomDepth = false;
 }
