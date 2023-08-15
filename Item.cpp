24ea42ca02bec5f7d267d8601eac27810d1f0005
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-08-15 13:30:14 +0300

    Dynamic Material Instances

--- Item.cpp
+++ Item.cpp
@@ -23,7 +23,8 @@ AItem::AItem() : ItemName(FString("Default")),
 				 ItemInterpY(0.f),
 				 InterpInitialYawOffset(0.f),
 				 ItemType(EItemType::EIT_MAX),
-				 InterpLocIndex(0)
+				 InterpLocIndex(0),
+				 MaterialIndex(0)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -293,6 +294,15 @@ void AItem::DisableCustomDepth()
 void AItem::InitializeCustomDepth()
 {
 	DisableCustomDepth();
+}
+
+void AItem::OnConstruction(const FTransform& Transform)
+{
+	if (MaterialInstance)
+	{
+		DynamicMaterialInstance = UMaterialInstanceDynamic::Create(MaterialInstance, this);
+		ItemMesh->SetMaterial(MaterialIndex, DynamicMaterialInstance);
+	}
 }
 
 // Called every frame
