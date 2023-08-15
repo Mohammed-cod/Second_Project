5e339daf87e2e70d1e7bfa557e68448d60e8dc6c
Mohammed-cod <80847500+Mohammed-cod@users.noreply.github.com>
2023-08-15 14:40:11 +0300

    Enable Glow Material in C++

--- Item.cpp
+++ Item.cpp
@@ -207,6 +207,9 @@ void AItem::FinishInterping()
 	}
 	// Set scale back to normal
 	SetActorScale3D(FVector(1.f));
+
+	DisableGlowMaterial();
+	DisableCustomDepth();
 }
 
 void AItem::ItemInterp(float DeltaTime)
@@ -303,7 +306,25 @@ void AItem::OnConstruction(const FTransform& Transform)
 		DynamicMaterialInstance = UMaterialInstanceDynamic::Create(MaterialInstance, this);
 		ItemMesh->SetMaterial(MaterialIndex, DynamicMaterialInstance);
 	}
+	EnableGlowMaterial();
 }
+
+void AItem::EnableGlowMaterial()
+{
+	if (DynamicMaterialInstance)
+	{
+		DynamicMaterialInstance->SetScalarParameterValue(TEXT("GlowBlendAlpha"), 0);
+	}
+}
+
+void AItem::DisableGlowMaterial()
+{
+	if (DynamicMaterialInstance)
+	{
+		DynamicMaterialInstance->SetScalarParameterValue(TEXT("GlowBlendAlpha"), 1);
+	}
+}
+
 
 // Called every frame
 void AItem::Tick(float DeltaTime)
